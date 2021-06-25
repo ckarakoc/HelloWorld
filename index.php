2f25bb1071910cb74a0b05728b710fd0a7f2053e
Celal Karakoç <c.karakoc@live.nl>
2021-06-25 21:55:00 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php include_once("home.html"); ?>
+<?php 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Max-Age: 600");
+header("Access-Control-Allow-Methods: GET");
+include_once("home.html");
+?>
