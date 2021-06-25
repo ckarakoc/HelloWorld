4bfa69ed474e86b02dec0d13e3e9b0dc34ca8114
Celal Karakoç <c.karakoc@live.nl>
2021-06-25 22:01:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Max-Age: 600");
 header("Access-Control-Allow-Methods: GET");
 include_once("home.html");
+echo "This is my config var: ";
+echo getenv("TEST");
 ?>
